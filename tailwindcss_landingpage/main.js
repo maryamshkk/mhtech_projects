b1bb3f1074db95a069cb449a0d8c5d9307968fbd
maryamshkk <maryamshk117@gmail.com>
2026-06-28 01:38:52 +0500

    Initial commit

---
+++ tailwindcss_landingpage/main.js
@@ -0,0 +1,131 @@
+// TOGGLE BUTTON 
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".navlink")
+const hamburger = document.getElementById("hamburger")
+
+// open/close menu 
+hamburger.addEventListener("click", () =>{
+    navMenu.classList.toggle("left-0")
+    navMenu.classList.toggle("-left-full")
+    
+
+    hamburger.classList.toggle("ri-menu-4-line")
+    hamburger.classList.toggle('ri-close-large-line')
+});
+
+// Close menu when a link is clicked
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.add("-left-full");
+        navMenu.classList.remove("left-0");
+
+        hamburger.classList.add("ri-menu-4-line");
+        hamburger.classList.remove("ri-close-large-line");
+    });
+});
+
+
+// SHOW SCROLL UP
+const scrollUp = () => {
+  const scrolUpBtn = document.getElementById("scroll-up")
+
+  if(this.scrollY >=250) {
+    scrolUpBtn.classList.remove("-bottom-1/2")
+    scrolUpBtn.classList.add("bottom-4")
+  } else {
+    scrolUpBtn.classList.add("-bottom-1/2")
+    scrolUpBtn.classList.remove("bottom-4")    
+  }
+}
+
+window.addEventListener("scroll", scrollUp)
+
+
+
+
+// CHANGED BACKGROUND HEADER
+const scrollHeader = () => {
+  const header = document.getElementById("navbar")
+
+  if(this.scrollY >=250) {
+    header.classList.add("border-b", "border-yellow-500")
+  } else {
+    header.classList.remove("border-b", "border-yellow-500")    
+  }
+}
+
+window.addEventListener("scroll", scrollHeader)
+
+// SWIPER
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween:30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false 
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable :true
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    }, 
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
+
+
+// SCROLL SECTIONS ACTIVE LINK 
+const activeLink = () => {
+  const sections = document.querySelectorAll('section')
+  const navLinks = document.querySelectorAll('.navlink')  // ✅ fixed
+
+  let current = "home"
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop
+    if (window.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id")
+    }
+  })
+
+  navLinks.forEach(item => {
+    item.classList.remove("active")
+    if (item.href.includes(current)) {
+      item.classList.add("active")
+    }
+  })
+}
+
+window.addEventListener("scroll", activeLink)
+
+// SCROLL REvEAL ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance : "60px",
+  duration: 2500,
+  delay: 300,
+  reset: true,
+})
+
+sr.reveal(`.home__data, .about__top, popular__top, .review__top, .review__swiper
+  .footer_icon, .footer__content, .copy__right`)
+sr.reveal(`.home__image`, {delay: 500, scale: 0.5})
+
+sr.reveal(`.service__card, .popular__card`, {interval: 100})
+
+
+sr.reveal(`.about__leaf`, {delay: 1000, origin: "right"})
+sr.reveal(`.about__item__1-content, .about__item__2-img`, {origin: "right"})
+sr.reveal(`.about__item__2-content, .about__item__1-img`, {origin: "left"})
+sr.reveal(`.review__leaf, .footer__floral`, {delay: 800, origin: "left"})
