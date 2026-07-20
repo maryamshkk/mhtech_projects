8524c434b4b8b11cc61aa4b9351057ef50e1eec0
maryamshkk <maryamshk117@gmail.com>
2026-07-20 16:24:23 +0500

    created get,post api

---
+++ laravel/laravel-rest-api/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+// all users
+Route::get('/users', [UserController::class, 'index']);
+
+// single user api
+Route::get('users/{id}', [UserController::class, 'show']);
+
+// create user api 
+Route::post('/users', [UserController::class, 'store']);
+
+// update user
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+// delete usr
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
