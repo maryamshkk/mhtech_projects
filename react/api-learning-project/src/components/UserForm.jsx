6a21c565e97ea1c8d2000d4f993cd239bacec44b
maryamshkk <maryamshk117@gmail.com>
2026-07-31 22:21:38 +0500

    form making, handle change, handle submit

---
+++ react/api-learning-project/src/components/UserForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function UserForm()
+{
+    const [formData, setFormData] = useState({
+
+    name: "",
+
+    email: ""
+
+});
+    function handleChange(e){
+
+    setFormData({
+
+        ...formData,
+
+        [e.target.name]: e.target.value
+
+    });
+
+
+
+}
+    function handleSubmit(e){
+
+        e.preventDefault();
+
+        console.log(formData);
+
+}
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+            
+            <h2>Add new User</h2>
+            
+            <input type="text"
+                name = "name"
+                placeholder="Enter Name"
+                value={formData.name}
+                onChange={handleChange}
+            />
+            <br></br>
+            <input type="email" 
+                name="email"
+                placeholder="Enter Email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <br></br>
+
+            <button>Add New User</button>
+            </form>
+        </div>
+    )
+}
+export default UserForm;
--- react/api-learning-project/src/pages/ApiUsers.jsx
+++ react/api-learning-project/src/pages/ApiUsers.jsx
@@ -1,30 +1,57 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
+import UserForm from "../components/UserForm";
 
 function ApiUsers()
 {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(()=>{
         // console.log("Compound Mounted");
 
-        async function fetchUsers()
-        {
+        async function fetchUsers() {
+
+        try {
+
             const response = await fetch(
                 "https://jsonplaceholder.typicode.com/users"
             );
-            const data = await response.json();
-            setUsers(data);
-            // console.log(data);
-        }
-        fetchUsers();
-    }, [])
 
+            const data = await response.json();
+
+            setUsers(data);
+
+        }
+        catch (error) {
+
+            setError("Failed to load users.");
+
+        }
+
+        setLoading(false);
+
+    }
+    fetchUsers();
+    
+}, [])
+        if (loading) {
+
+        return <h1>Loading...</h1>;
+        }
+        if (error) {
+
+        return <h1>{error}</h1>;
+    
+        }
     return(
         <>
             <h1>
                 API Learning
             </h1>
+            <br></br>
+                <UserForm />
             {
                 users.map((user)=>{
                     return(
@@ -38,8 +65,12 @@ function ApiUsers()
             
             
             }
+            <br></br>
+
+
         </>
     )
 }
+
 
 export default ApiUsers;
