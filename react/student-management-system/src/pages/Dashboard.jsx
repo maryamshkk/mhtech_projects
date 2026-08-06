6fe8c47dc406f9f6248e789655f18a2c076b9e1b
maryamshkk <maryamshk117@gmail.com>
2026-08-06 18:11:04 +0500

    Update Dashboard page

--- react/student-management-system/src/pages/Dashboard.jsx
+++ react/student-management-system/src/pages/Dashboard.jsx
@@ -106,7 +106,10 @@ function Dashboard()
             try{
 
             const response = await fetch(
-                "https://jsonplaceholder.typicode.com/users"
+                "https://jsonplaceholder.typicode.com/users",
+                {
+                    method:"POST"
+                }
                );
             const data = await response.json();
 
