05a22ea153a6419b7a8a9054a883c30702d228e9
maryamshkk <maryamshk117@gmail.com>
2026-08-06 20:19:51 +0500

    prisma client generated

--- prisma/index.js
+++ prisma/index.js
@@ -1,1 +1,21 @@
-import { PrismaC } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    // insert data 
+    // create single user
+    const user = await prisma.user.create({
+        data:{
+            name: "maryam",
+            email: "maryamsheikh117@gmail.com"
+        },
+    });
+    console.log(user);
+};
+
+main()
+    .catch((e)=> console.error(e))
+    .finally(async () =>{
+        await prisma.$disonnect();
+    })
