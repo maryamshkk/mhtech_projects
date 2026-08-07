4c6f7f6a539d6706798ba61ef20330c4eb8bee4b
maryamshkk <maryamshk117@gmail.com>
2026-08-07 21:38:51 +0500

    insert multiple students in database

--- prisma/crud/index.js
+++ prisma/crud/index.js
@@ -1,2 +1,12 @@
-// const express = require("express");
-// const app = express();
+const req = require("express/lib/request");
+const prisma = require("./prisma");
+
+await prisma.student.createMany({
+    data:[
+        { name: "a", email: "a@gmail.com", age: 22, cgpa: 3.4},
+        { name: "b", email: "b@gmail.com", age: 23, cgpa: 3.3},
+        { name: "c", email: "c@gmail.com", age: 24, cgpa: 3.2},
+        { name: "d", email: "d@gmail.com", age: 25, cgpa: 3.1},
+        { name: "e", email: "e@gmail.com", age: 26, cgpa: 3.0}, 
+]
+})
