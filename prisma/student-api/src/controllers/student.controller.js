53b202517c8316bab9621f48fa0633ff7a5c483c
maryamshkk <maryamshk117@gmail.com>
2026-08-10 23:00:05 +0500

    get single id

--- prisma/student-api/src/controllers/student.controller.js
+++ prisma/student-api/src/controllers/student.controller.js
@@ -36,3 +36,26 @@ export const createStudent = async (req, res) =>{
         });
     }
 }
+
+// get student by id 
+export const getStudent = async (req,res) => {
+    try {
+        const id = Number(req.params.id);
+
+        const student = await prisma.student.findUnique({
+            where: {id:id}
+        });
+
+        if (!student) {
+            return res.status(404).json({
+                message: "student not found"
+            })
+        }
+        res.status(200).json(student);
+    }
+    catch (error){
+        res.status(500).json({
+            message: error.message
+        });
+    }
+} 
--- prisma/student-api/src/routes/student.routes.js
+++ prisma/student-api/src/routes/student.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getStudents, createStudent
-
+import { getStudents, createStudent, getStudent
  } from "../controllers/student.controller.js";
 
 const router = express.Router();
 
 router.get("/", getStudents);
 router.post("/", createStudent);
+router.get("/:id", getStudent);
 
 export default router;
